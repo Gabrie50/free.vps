08da2548ea68e2de6c352316702432ad43af1701
Gabrie50 <gcriste268@gmail.com>
2025-11-03 11:21:19 -0300

    Update VPSFREE script for XFCE4 and PufferPanel

--- vpsfree.sh
+++ vpsfree.sh
@@ -3,6 +3,8 @@ clear
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 RED='\033[0;31m'
+NC='\033[0m'
+
 echo "
 #######################################################################################
 #
@@ -12,8 +14,9 @@ echo "
 #
 #
 #######################################################################################"
+
 echo "Select an option:"
-echo "1) LXDE - XRDP"
+echo "1) XFCE4 Minimal - XRDP"
 echo "2) PufferPanel"
 echo "3) Install Basic Packages"
 echo "4) Install Nodejs"
@@ -23,21 +26,29 @@ if [ $option -eq 1 ]; then
     clear
     echo -e "${RED}Downloading... Please Wait"
     apt update && apt upgrade -y
+
+    # Remover sudo como no script original
     export SUDO_FORCE_REMOVE=yes
     apt remove sudo -y
-    apt install lxde -y
-    apt install xrdp -y
-    echo "lxsession -s LXDE -e LXDE" >> /etc/xrdp/startwm.sh
-clear
-    echo -e "${GREEN}Downloading and installation completed!"
+
+    # Instalar XFCE4 minimal
+    apt install xfce4 xfce4-goodies xrdp -y
+
+    # Configurar XRDP para iniciar XFCE4
+    echo "startxfce4" > /etc/xrdp/startwm.sh
+    chmod +x /etc/xrdp/startwm.sh
+
+    clear
+    echo -e "${GREEN}XFCE4 minimal + XRDP installation completed!"
     echo -e "${YELLOW}Select RDP Port"
     read selectedPort
 
     sed -i "s/port=3389/port=$selectedPort/g" /etc/xrdp/xrdp.ini
-clear
+    clear
     service xrdp restart
     clear
     echo -e "${GREEN}RDP Created And Started on Port $selectedPort"
+
 elif [ $option -eq 2 ]; then
     clear
     echo -e "${RED}Downloading... Please Wait"
@@ -49,7 +60,7 @@ elif [ $option -eq 2 ]; then
     apt update && apt upgrade -y
     curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py
     chmod -R 777 /bin/systemctl
-    apt install pufferpanel
+    apt install pufferpanel -y
     clear
     echo -e "${GREEN}PufferPanel installation completed!"
     echo -e "${YELLOW}Enter PufferPanel Port"
@@ -69,74 +80,54 @@ elif [ $option -eq 2 ]; then
     systemctl restart pufferpanel
     clear
     echo -e "${GREEN}PufferPanel Created & Started - PORT: ${NC}$pufferPanelPort${GREEN}"
+
 elif [ $option -eq 3 ]; then
-clear
+    clear
     echo -e "${RED}Downloading... Please Wait"
-     apt update && apt upgrade -y
-     apt install git curl wget sudo lsof iputils-ping -y
-     curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py
-     chmod -R 777 /bin/systemctl
-     clear
+    apt update && apt upgrade -y
+    apt install git curl wget sudo lsof iputils-ping -y
+    curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py
+    chmod -R 777 /bin/systemctl
+    clear
     echo -e "${GREEN}Basic Packages Installed!" 
     echo -e "${RED}sudo / curl / wget / git / lsof / ping"
+
 elif [ $option -eq 4 ]; then
-#!/bin/bash
+    echo "Choose a Node.js version to install:"
+    echo "1. 12.x"
+    echo "2. 13.x"
+    echo "3. 14.x"
+    echo "4. 15.x"
+    echo "5. 16.x"
+    echo "6. 17.x"
+    echo "7. 18.x"
+    echo "8. 19.x"
+    echo "9. 20.x"
 
-echo "Choose a Node.js version to install:"
-echo "1. 12.x"
-echo "2. 13.x"
-echo "3. 14.x"
-echo "4. 15.x"
-echo "5. 16.x"
-echo "6. 17.x"
-echo "7. 18.x"
-echo "8. 19.x"
-echo "9. 20.x"
+    read -p "Enter your choice (1-9): " choice
 
-read -p "Enter your choice (1-9): " choice
+    case $choice in
+        1) version="12" ;;
+        2) version="13" ;;
+        3) version="14" ;;
+        4) version="15" ;;
+        5) version="16" ;;
+        6) version="17" ;;
+        7) version="18" ;;
+        8) version="19" ;;
+        9) version="20" ;;
+        *) echo "Invalid choice. Exiting."; exit 1 ;;
+    esac
 
-case $choice in
-    1)
-        version="12"
-        ;;
-    2)
-        version="13"
-        ;;
-    3)
-        version="14"
-        ;;
-    4)
-        version="15"
-        ;;
-    5)
-        version="16"
-        ;;
-    6)
-        version="17"
-        ;;
-    7)
-        version="18"
-        ;;
-    8)
-        version="19"
-        ;;
-    9)
-        version="20"
-        ;;
-    *)
-        echo "Invalid choice. Exiting."
-        exit 1
-        ;;
-esac
-echo -e "${RED}Downloading... Please Wait"
-apt remove --purge node* nodejs npm -y
-apt update && apt upgrade -y && apt install curl -y
-curl -sL "https://deb.nodesource.com/setup_${version}.x" -o /tmp/nodesource_setup.sh
-bash /tmp/nodesource_setup.sh
-apt update -y
-apt install -y nodejs
-clear
-echo -e "${GREEN}Node.js version $version has been installed."
+    echo -e "${RED}Downloading... Please Wait"
+    apt remove --purge node* nodejs npm -y
+    apt update && apt upgrade -y && apt install curl -y
+    curl -sL "https://deb.nodesource.com/setup_${version}.x" -o /tmp/nodesource_setup.sh
+    bash /tmp/nodesource_setup.sh
+    apt update -y
+    apt install -y nodejs
+    clear
+    echo -e "${GREEN}Node.js version $version has been installed."
 
 else
     echo -e "${RED}Invalid option selected.${NC}"
